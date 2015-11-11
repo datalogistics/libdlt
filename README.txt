4c0dc364854a207fdf0bee25efe15b0cc10444e8
Prakash <prakraja@umail.iu.edu>
2015-11-11 17:48:04 -0500

    Adding a README

---
+++ README.txt
@@ -0,0 +1,32 @@
+				________
+
+				 README
+				________
+
+Table of Contents
+_________________
+
+1 dlt tools
+.. 1.1 Introduction
+.. 1.2 Arguments
+
+
+1 dlt tools
+===========
+
+1.1 Introduction
+~~~~~~~~~~~~~~~~
+
+  Basically contains 2 Clis
+  - landsat_listner : Listen for new scenes from host using websocket
+    and download when a new file is uploaded
+  - landsat_download : Download the existing scenes specified in the
+    list
+
+
+1.2 Arguments
+~~~~~~~~~~~~~
+
+  It contains the following arguments:
+  - -s , --scenes : comma separated list of scenes
+  - -H , --host : host to get scenes from Eg: [http://dev.crest.iu.edu:8888/exnodes]
--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
+    print os.path.join(os.path.dirname(__file__), fname)
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
@@ -19,12 +20,12 @@ setup(
     url = "https://github.com/datalogistics/dlt-misc",
     packages=['tools'],
     package_data={'tools' : ['*']},
-    long_description="Dlt tools",
+    long_description=read("README.txt"),
     include_package_data = True,
     install_requires=[
         "websocket-client>=0.34",
         "requests",
-        "six>=1.6"
+        "six>=1.10.0"
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
