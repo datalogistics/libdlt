c48b49c7243dc3c5c2e93d94fadc51f682331c49
Prakash <prakraja@umail.iu.edu>
2016-03-09 13:39:57 -0500

    SSL works

--- tools/cli.py
+++ tools/cli.py
@@ -11,7 +11,8 @@ import requests
 from subprocess import Popen
 import cmdparser
 import urllib
-
+from functools import partial
+from urlparse import urlparse
 ## Re
 def signal_handler(signal, frame):
     print('Exiting the program')
@@ -37,8 +38,9 @@ def completer(text, state):
 readline.set_completer(completer)
 readline.parse_and_bind("tab: complete")
 map = {}
-def display(str) :
-    print str
+def display(s) :
+    print str(s)
+
 def display_error(str) :
     print str
 
@@ -64,11 +66,72 @@ def showUnisList(arr,index) :
     for i in unislist:
         display(i + "-> " + get_url(unislist[i]))
 
+def get (url, cb , cert=None,errorCb = None) :
+    """ Get json from url and return the processed data through cb or errorCb """
+    try:
+        if not cert or not cert['ssl']:
+            r = (requests.get(url)).json()
+        else :
+            r = (requests.get(url,cert=(cert['cert'],cert['key']))).json()
+        return cb(r)
+    except Exception as e:
+        print "Error geting url ", url , "\n"
+        if errorCb :
+            return errorCb(url)
+        else :
+            print e
+
+def processUnisTop (json) :
+    """ Process unis top level and get list of commands """
+    cmd = {}
+    for i in json :
+        o = urlparse(i['href'])
+        name  = o.path[1:]  # remove /
+        cmd[name] = { 'f' : (lambda x,y : get(i['href'],display))}
+    return cmd
+
+def getQueryParams (arr) :
+    """ consume as many as required and recursively solve it """
+    s = ""
+    # Index based
+    complist = ['>','<','=','<=','>=','!=','reg','and','or']
+    opmap = {'!=': 'not=eq=',
+         '<': 'lt',
+         '<=': 'lte',
+         '=': 'eq',
+         '>': 'gt',
+         '>=': 'gte',
+         'and': 'and',
+         'or': 'or',
+         'reg': 'reg'}
+    showHelp = False
+    while len(arr) > 1 :
+        if arr[0] == '?' :
+            showHelp = True
+        elif arr[0] in opmap :
+            s += opmap[arr[0]] + "=" + arr[1] + "&"
+        arr = arr[2:]
+    return s , showHelp
+
+def getUnisListCommands(unisob,arr,ind) :
+    query, showHelp = getQueryParams(arr[ind:])
+    return [], 1 , get(get_url(unisob)+"?"+query,processUnisTop,unisob)
+
+def getUnisList(arr,index) :
+    cmd = {}
+    for i in unislist:
+        cmd[i] = { 'getCommands' : partial(getUnisListCommands,unislist[i]) }
+    return arr , index , cmd
+
+# Can contain either f , getCommands or <command names> (recurrsively) . f terminates sequence
 commands = {
     "list" : {
         'unis' : { 'f' : showUnisList },
         'blipp' : {},
         'harvester' : {}
+    },
+    'cd' : {
+        'getCommands' : getUnisList,
     },
     "show" : { },
     "exit" : { 'f' : (lambda x,y : sys.exit(0)) }
@@ -121,7 +184,7 @@ def parseCommands (arr,index,cmd) :
         parseCommands(narr,nind,ncmd) if ncmd else None
     elif index >= len(arr):
         """ Maybe just end of stuff to do - Show as nothing to do """
-        display("Nothing to do - Probably incomplete functionality")
+        # display("Nothing to do - Probably incomplete functionality")
         showHelp(arr,index,cmd)
     elif arr[index] in cmd :
         parseCommands(arr,index + 1,cmd[arr[index]])
