2fe976ba058e0d2ebf5a2ce6609b25b0c91e08af
jayaajay <jayaajay@indiana.edu>
2016-07-07 12:36:43 -0400

    Removing the license header from json files

--- scripts/listener_loader.sh
+++ scripts/listener_loader.sh
@@ -1,13 +1,1 @@
-# =============================================================================
-#  Data Logistics Toolkit (dlt-tools)
-#
-#  Copyright (c) 2015-2016, Trustees of Indiana University,
-#  All rights reserved.
-#
-#  This software may be modified and distributed under the terms of the BSD
-#  license.  See the COPYING file for details.
-#
-#  This software was created at the Indiana University Center for Research in
-#  Extreme Scale Technologies (CREST).
-# =============================================================================
 python /home/Jeremy/bddlt/misc/landsat_listener.py --scene LC80430332013243LGN00,LC80430342013243LGN00,LC80420352013236LGN00;
--- scripts/slow_download.sh
+++ scripts/slow_download.sh
@@ -1,13 +1,1 @@
-# =============================================================================
-#  Data Logistics Toolkit (dlt-tools)
-#
-#  Copyright (c) 2015-2016, Trustees of Indiana University,
-#  All rights reserved.
-#
-#  This software may be modified and distributed under the terms of the BSD
-#  license.  See the COPYING file for details.
-#
-#  This software was created at the Indiana University Center for Research in
-#  Extreme Scale Technologies (CREST).
-# =============================================================================
 lors_download -X http://dlt.incntre.iu.edu:42424 -V 1 -t 10 -b 5m -f http://dev.incntre.iu.edu:8888/exnodes/551086eee7798953b6f42521 -o far.tar.gz
