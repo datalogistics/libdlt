01a595e494465615a60898c80a2513476e01c5f3
Ezra Kissel <ezkissel@indiana.edu>
2016-10-14 14:41:30 -0400

    Additions to restore IBP protocol backend, using unisrt.

---
+++ libdlt/depot.py
@@ -0,0 +1,39 @@
+from uritools import urisplit
+
+class Depot():
+    def __init__(self, uri):
+        try:
+            o = urisplit(uri)
+            self.authority = o.authority
+            self.uri = o.geturi()
+            self.scheme = o.getscheme(default=None)
+            self.host = o.gethost(default=None)
+            self.port = o.getport(default=None)
+        except Exception as e:
+            raise e
+
+        @property
+        def uri(self):
+            return self.uri
+        
+        @property
+        def endpoint(self):
+            return "{0}://{1}".format(self.scheme, self.authority)
+        
+        @property
+        def scheme(self):
+            return self.scheme
+
+        @property
+        def host(self):
+            return self.host
+
+        @property
+        def port(self):
+            return self.port
+
+        def __repr__(self):
+            return self.uri
+        
+        def __str__(self):
+            return self.uri
