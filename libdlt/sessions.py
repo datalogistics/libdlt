03c166d8ba1923bdfdb9c16a9b56bd7f6e1e898f
Jeremy Musser <jemusser@indiana.edu>
2016-11-16 18:19:00 -0600

    Removed return from coroutines

--- libdlt/sessions.py
+++ libdlt/sessions.py
@@ -243,7 +243,7 @@ class Session(object):
         time_s = time.time()
         self._jobs.put_nowait((0, ex.size))
         workers = [asyncio.ensure_future(self._download_chunks(filepath, schedule, sock), loop=self._loop) for _ in range(self._threads)]
-        done, pending = self._loop.run_until_complete(asyncio.wait(workers))
+        self._loop.run_until_complete(asyncio.wait(workers))
         
         return (time.time() - time_s, ex)
         
