04b89192fad05e21c4331c8e6e1350ed71e5ece9
Jeremy Musser <jemusser@indiana.edu>
2018-10-01 16:48:03 -0400

    Reset unisrt dependency to @develop

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     ],
     dependency_links=[
         "git+https://github.com/periscope-ps/lace.git/@master#egg=lace",
-        "git+https://github.com/periscope-ps/unisrt.git/@multisource#egg=unisrt"
+        "git+https://github.com/periscope-ps/unisrt.git/@develop#egg=unisrt"
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
